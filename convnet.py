5fa9543e71557b94432ed017d8b0656b07f83122
Hendrik Weideman <weideh@rpi.edu>
2015-07-02 23:10:13 -0400

    Rough first outline of a 1D-CNN.

---
+++ convnet.py
@@ -0,0 +1,114 @@
+from lasagne import layers
+from lasagne import nonlinearities
+from lasagne import init
+from lasagne.layers import cuda_convnet
+
+#Conv1DLayer = cuda_convnet.Conv1DCCLayer
+#MaxPool1DLayer = cuda_convnet.MaxPool1DCCLayer
+
+Conv1DLayer = cuda_convnet.Conv1DCCLayer
+MaxPool1DLayer = cuda_convnet.MaxPool1DCCLayer
+
+
+def build_model(batch_size,
+                num_channels,
+                input_length,
+                output_dim,):
+    l_in = layers.InputLayer(
+        shape=(batch_size, num_channels, input_length),
+        name='l_in',
+    )
+
+    l_conv1 = Conv1DLayer(
+        l_in,
+        name='conv1',
+        num_filters=32,
+        border_mode='valid',
+        filter_size=3,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_pool1 = MaxPool1DLayer(
+        l_conv1,
+        name='pool1',
+        pool_size=3,
+        stride=2,
+    )
+
+    l_conv2 = Conv1DLayer(
+        l_pool1,
+        name='conv2',
+        num_filters=64,
+        border_mode='valid',
+        filter_size=3,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_pool2 = MaxPool1DLayer(
+        l_conv2,
+        name='pool2',
+        pool_size=(3, 3),
+        stride=(2, 2),
+    )
+
+    l_conv3 = Conv1DLayer(
+        l_pool2,
+        name='conv3',
+        num_filters=128,
+        border_mode='valid',
+        filter_size=3,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_conv4 = Conv1DLayer(
+        l_conv3,
+        name='conv4',
+        num_filters=128,
+        border_mode='valid',
+        filter_size=3,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_pool4 = MaxPool1DLayer(
+        l_conv4,
+        name='pool4',
+        pool_size=3,
+        stride=2,
+    )
+
+    l_dropout_dense1 = layers.DropoutLayer(
+        l_pool4,
+        p=0.5,
+    )
+
+    l_dense1 = layers.DenseLayer(
+        l_dropout_dense1,
+        num_units=512,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_dropout_dense2 = layers.DropoutLayer(
+        l_dense1,
+        p=0.5,
+    )
+
+    l_dense2 = layers.DenseLayer(
+        l_dropout_dense2,
+        num_units=512,
+        nonlinearity=nonlinearities.rectify,
+        W=init.Orthogonal(),
+    )
+
+    l_out = layers.DenseLayer(
+        l_dense2,
+        num_units=output_dim,
+        nonlinearity=nonlinearities.sigmoid,
+        W=init.Orthogonal(),
+    )
+
+    return l_out
