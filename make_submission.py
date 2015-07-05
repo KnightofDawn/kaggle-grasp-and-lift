a84e5d4e143cec26dce2f19d17f59d356e5b70e1
Hendrik Weideman <weideh@rpi.edu>
2015-07-05 19:20:47 -0400

    Code to use the different models to generate prediction files and then concatenate the files to make the final submission.

---
+++ make_submission.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+def make_submission():
+    subjects = range(1, 13)
+    submission_file = 'data/submissions/convnet.csv'
+    header = ['HandStart', 'FirstDigitTouch', 'BothStartLoadPhase',
+              'LiftOff', 'Replace', 'BothReleased']
+    print('generating submission file: %s' % (submission_file))
+    with open(submission_file, 'w') as ofile:
+        ofile.write('%s\n' % header)
+        for subj_id in subjects:
+            pred_file = 'data/predictions/subj%d_preds.csv' % (subj_id)
+            print('  reading probabilities from %s' % (pred_file))
+            with open(pred_file, 'r') as ifile:
+                for line in ifile:
+                    ofile.write(line)
+
+
+def main():
+    make_submission()
+
+if __name__ == '__main__':
+    main()
