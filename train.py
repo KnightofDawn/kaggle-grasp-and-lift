Fixed bug in normalization that allows to train convnets without CSP.

train.py
@@ -41,16 +41,14 @@ def train_model(subj_id, window_size, subsample, max_epochs):
     #    utils.preprocess(subj_id, train_data, valid_data, compute_csp=True)
     train_data, valid_data = \
         utils.preprocess(subj_id, train_data, valid_data,
-                         compute_csp=True,
-                         butter_smooth=True,
-                         boxcar_smooth=True)
+                         compute_csp=False, nfilters=2,
+                         butter_smooth=False,
+                         boxcar_smooth=False)
 
-    #batch_size = 512
-    #batch_size = 128
     batch_size = 16
     # remember to change the number of channels when there is csp!!!
-    num_channels = 4
-    #num_channels = 32
+    #num_channels = 2
+    num_channels = 32
     num_actions = 6
     print('building model...')
     l_out = build_model(None, num_channels,
@@ -89,10 +87,9 @@ def train_model(subj_id, window_size, subsample, max_epochs):
     best_weights = None
     best_valid_loss = np.inf
     best_epoch = 0
-    #sampling = np.array(range(0, 500, 32) +  # 16 numbers
-    #                    range(500, 1000, 15) +  # 34 numbers
-    #                    range(1000, 1500, 10) +  # 50 numbers
-    #                    range(1500, 2000, 5))  # 100 numbers
+    #sampling = np.array(range(0, 400, 10) +
+    #                    range(400, 800, 4) +
+    #                    range(800, 1000, 2))
     try:
         for epoch in range(max_epochs):
             print('epoch: %d' % (epoch))
@@ -158,6 +155,9 @@ def train_model(subj_id, window_size, subsample, max_epochs):
                     print('nan loss encountered in minibatch %d' % (i))
                     continue
 
+                if np.isnan(valid_loss):
+                    print('nan loss encountered in minibatch %d' % (i))
+                    continue
                 if (i + 1) % 10000 == 0:
                     print('    processed validation minibatch %d of %d...' %
                           (i + 1, num_batches))

utils.py
@@ -45,10 +45,10 @@ def load_subject_test(subj_id):
     return data_list, ids_list
 
 
-def preprocess(subj_id, train_data, test_data, compute_csp=True,
-               nfilters=4, butter_smooth=True, boxcar_smooth=True):
-    train_data = [1e-6 * data for data in train_data]
-    test_data = [1e-6 * data for data in test_data]
+def preprocess(subj_id, train_data, test_data, compute_csp=True, nfilters=4,
+               butter_smooth=True, boxcar_smooth=True):
+    #train_data = [1e-6 * data for data in train_data]
+    #test_data = [1e-6 * data for data in test_data]
 
     if butter_smooth:
         b, a = butter(5, np.array([7, 30]) / 250., btype='bandpass')
@@ -71,10 +71,15 @@ def preprocess(subj_id, train_data, test_data, compute_csp=True,
     test_data = [data.astype(np.float32) for data in test_data]
 
     print('normalizing...')
+    # subtract the mean from all time series
     train_mean = np.mean(np.hstack(train_data), axis=1).reshape(-1, 1)
     train_data = [data - train_mean for data in train_data]
-    train_std = np.std(np.hstack(train_data), axis=1).reshape(-1, 1)
 
+    # divide all time series by the standard deviation
+    train_std = np.std(np.hstack(train_data), axis=1).reshape(-1, 1)
+    train_data = [data / train_std for data in train_data]
+
+    # apply the same transform to valid/test
     test_data = [data - train_mean for data in test_data]
     test_data = [data / train_std for data in test_data]
 