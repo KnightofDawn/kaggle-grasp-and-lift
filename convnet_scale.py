c2fa507171b9935af077a2a4b9acbc3dd6ca8f29
Hendrik Weideman <weideh@rpi.edu>
2015-07-13 17:02:44 -0400

    Improvements to the SubsamplingLayer.

--- convnet_scale.py
+++ convnet_scale.py
@@ -13,18 +13,34 @@ MaxPool1DLayer = pool.MaxPool1DLayer
 
 
 class SubsampleLayer(layers.Layer):
-    def __init__(self, incoming, window=(None, None, None), **kwargs):
-        super(layers.Layer, self).__init__(incoming, **kwargs)
-        self.window = window
+    def __init__(self, incoming, window, name=None, **kwargs):
+        super(SubsampleLayer, self).__init__(incoming, **kwargs)
+        if not isinstance(window, slice):
+            self.window = slice(*window)
+        else:
+            self.window = window
 
     def get_output_shape_for(self, input_shape):
         output_shape = list(input_shape)
-        output_shape[2] = input_shape[2] / self.window[2]
+        if self.window.stop is None and self.window.start is None:
+            output_shape[2] = input_shape[2] / self.window.step
+        elif self.window.stop is None:
+            output_shape[2] = (input_shape[2] - self.window.start) / self.window.step
+        elif self.window.start is None:
+            output_shape[2] = self.window.stop / self.window.step
+        else:
+            output_shape[2] = (self.window.stop - self.window.start) / self.window.step
         return tuple(output_shape)
 
     def get_output_for(self, input, **kwargs):
-        start, stop, step = self.window
-        return input[:, start:stop, (step - 1)::(step)]
+        step = self.window.step
+        start = (input.shape[0] - 1) % step
+        if self.window.start is not None:
+            start += self.window.start
+        stop = self.window.stop
+
+        s = slice(start, stop, step)
+        return input[:, :, s]
 
 
 def build_model(batch_size,
@@ -39,11 +55,13 @@ def build_model(batch_size,
     l_ss_left = SubsampleLayer(
         l_in,
         window=(None, None, 10),
+        name='l_ss_left',
     )
 
     l_ss_right = SubsampleLayer(
         l_in,
-        window=(1000, None, 5)
+        window=(1000, None, 5),
+        name='l_ss_right',
     )
 
     l_conv1_left = Conv1DLayer(
@@ -206,6 +224,7 @@ def build_model(batch_size,
 
     l_concat = layers.ConcatLayer(
         incomings=(l_pool4_left, l_pool4_right),
+        name='concat',
     )
 
     l_dropout_dense1 = layers.DropoutLayer(
