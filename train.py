093a3a65ffba7136e2a72329d2ec8a3d570cdb55
Hendrik Weideman <weideh@rpi.edu>
2015-08-14 17:06:03 -0400

    Show column-wise loss and roc scores.

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import sys
 
 from lasagne import layers
 from os.path import join
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, log_loss
 from time import time
 
 import batching
@@ -16,12 +16,13 @@ import utils
 
 #from convnet import build_model
 #from convnet_small import build_model
-from convnet_deep import build_model
+#from convnet_deep import build_model
 #from convnet_nopool import build_model
 #from convnet_deep_drop import build_model
 #from convnet_deep_drop_reg import build_model
-#from convnet_deep_scale import build_model
 #from convnet_bn import build_model
+from convnet_csp import build_model
+#from convnet_lr import build_model
 
 
 def train_model(subj_id, window_size, max_epochs, patience):
@@ -30,10 +31,12 @@ def train_model(subj_id, window_size, max_epochs, patience):
     #init_file = join(root_dir,
     #                 'subj%d_weights.pickle' % (
     #                     subj_id))
+    #init_file = join(root_dir,
+    #                 'weights_deep.pickle')
     init_file = None
     # the file to which the learned weights will be written
     weights_file = join(root_dir,
-                        'subj%d_noise.pickle' % (
+                        'subj%d_csp_lr.pickle' % (
                             subj_id))
     print('loading time series for subject %d...' % (subj_id))
     data_list, events_list = utils.load_subject_train(subj_id)
@@ -47,8 +50,6 @@ def train_model(subj_id, window_size, max_epochs, patience):
 
     train_data, valid_data = \
         utils.preprocess(train_data, valid_data)
-    #train_data = [data.astype(np.float32) for data in train_data]
-    #valid_data = [data.astype(np.float32) for data in valid_data]
 
     #train_data, train_events = utils.remove_easy_negatives(
     #    train_data, train_events)
@@ -65,10 +66,11 @@ def train_model(subj_id, window_size, max_epochs, patience):
     print('there are %d windows for training' % (len(train_slices)))
     print('there are %d windows for validation' % (len(valid_slices)))
 
-    train_slices = utils.duplicate_positives(train_slices, train_events)
-    print('there are %d windows for training' % (len(train_slices)))
+    #train_slices = utils.duplicate_positives(train_slices, train_events, N=10)
+    #print('there are %d windows for training' % (len(train_slices)))
 
     batch_size = 64
+    #batch_size = 128
     #batch_size = 256
     #batch_size = 4096
     num_channels = 32
@@ -104,6 +106,7 @@ def train_model(subj_id, window_size, max_epochs, patience):
 
     #lr = theano.shared(np.cast['float32'](0.001))
     lr = theano.shared(np.cast['float32'](0.01))
+    #lr = theano.shared(np.cast['float32'](0.1))
     mntm = 0.9
     print('compiling theano functions...')
     train_iter = iter_funcs.create_iter_funcs_train(lr, mntm, l_out)
@@ -125,7 +128,7 @@ def train_model(subj_id, window_size, max_epochs, patience):
                                         train_slices,
                                         train_data,
                                         train_events,
-                                        noisy=True)):
+                                        noisy=False)):
                 t_batch_start = time()
                 # hack for faster debugging
                 #if i < 70000:
@@ -140,7 +143,7 @@ def train_model(subj_id, window_size, max_epochs, patience):
                     eta = batch_duration * (num_batches - i)
                     m, s = divmod(eta, 60)
                     h, m = divmod(m, 60)
-                    print('  training...  (ETA = %d:%02d:%02d)\r'
+                    print('  training...    (ETA = %d:%02d:%02d)\r'
                           % (h, m, s)),
                 sys.stdout.flush()
                 train_losses.append(train_loss)
@@ -153,12 +156,23 @@ def train_model(subj_id, window_size, max_epochs, patience):
             training_inputs = np.vstack(training_inputs)
             training_outputs = np.vstack(training_outputs)
             train_roc = roc_auc_score(training_inputs, training_outputs)
+            train_rocs = [roc_auc_score(
+                training_inputs[:, i], training_outputs[:, i])
+                for i in range(training_inputs.shape[1])]
+
+            train_losses = [log_loss(
+                training_inputs[:, i], training_outputs[:, i])
+                for i in range(training_inputs.shape[1])]
 
             train_duration = time() - t_train_start
             print('')
-            print('    train loss: %.6f' % (avg_train_loss))
-            print('    train roc:  %.6f' % (train_roc))
-            print('    duration:   %.2f s' % (train_duration))
+            print('    train loss:  %.6f (%s)' % (
+                avg_train_loss, ', '.join(['%.3f' % (loss)
+                                          for loss in train_losses])))
+            print('    train roc:   %.6f (%s)' % (
+                train_roc, ', '.join(['%.3f' % (roc)
+                                     for roc in train_rocs])))
+            print('    duration:    %.2f s' % (train_duration))
 
             valid_losses, valid_outputs, valid_inputs = [], [], []
             num_batches = (len(valid_slices) + batch_size - 1) / batch_size
@@ -205,11 +219,23 @@ def train_model(subj_id, window_size, max_epochs, patience):
                 valid_inputs = np.vstack(valid_inputs)
                 valid_outputs = np.vstack(valid_outputs)
                 valid_roc = roc_auc_score(valid_inputs, valid_outputs)
+                valid_rocs = [roc_auc_score(
+                    valid_inputs[:, i], valid_outputs[:, i])
+                    for i in range(valid_inputs.shape[1])]
+
+                valid_losses = [log_loss(
+                    valid_inputs[:, i], valid_outputs[:, i])
+                    for i in range(valid_inputs.shape[1])]
+
                 valid_duration = time() - t_valid_start
                 print('')
-                print('    valid loss: %.6f' % (avg_valid_loss))
-                print('    valid roc:  %.6f' % (valid_roc))
-                print('    duration:   %.2f s' % (valid_duration))
+                print('    valid loss:  %.6f (%s)' % (
+                    avg_valid_loss, ', '.join(['%.3f' % (loss)
+                                              for loss in valid_losses])))
+                print('    valid roc:   %.6f (%s)' % (
+                    valid_roc, ', '.join(['%.3f' % (roc)
+                                         for roc in valid_rocs])))
+                print('    duration:    %.2f s' % (valid_duration))
             else:
                 print('    no validation...')
 
@@ -250,12 +276,13 @@ def train_model(subj_id, window_size, max_epochs, patience):
 
 def main():
     #subjects = range(5, 13)
-    subjects = [1]
+    #subjects = range(6, 13)
+    subjects = [5, 8, 12]
     window_size = 2000
     #window_size = 200
     #window_size = 1000
-    max_epochs = 10
-    patience = 2
+    max_epochs = 5
+    patience = 1
     #max_epochs = 5
     model_train_losses, model_valid_losses = [], []
     model_train_rocs, model_valid_rocs = [], []
